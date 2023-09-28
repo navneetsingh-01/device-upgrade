33c742eb4b68d19175ee45351cffc15842aad990
Navneet Singh <singhnavneet@salesforce.com>
2023-09-28 09:55:43 +0530

    precheck setup

--- test.py
+++ test.py
@@ -14,46 +14,46 @@ print("rc: {}".format(precheck_out))
 print("out: {}".format(precheck_err))
 print("err: {}".format(precheck_rc))
 
-if precheck_rc == 0:
-    prestage_out, prestage_err, prestage_rc = ansible_runner.run_command(
-        executable_cmd='ansible-playbook',
-        cmdline_args=['/home/singhnavneet.su/os-upgrade/project/prestage.yaml',
-                      '-i', 'inventory', '-vvvv', '--tags', '9k'],
-        input_fd=sys.stdin,
-        output_fd=sys.stdout,
-        error_fd=sys.stderr,
-    )
-    print("rc: {}".format(prestage_out))
-    print("out: {}".format(prestage_err))
-    print("err: {}".format(prestage_rc))
+# if precheck_rc == 0:
+#     prestage_out, prestage_err, prestage_rc = ansible_runner.run_command(
+#         executable_cmd='ansible-playbook',
+#         cmdline_args=['/home/singhnavneet.su/os-upgrade/project/prestage.yaml',
+#                       '-i', 'inventory', '-vvvv', '--tags', '9k'],
+#         input_fd=sys.stdin,
+#         output_fd=sys.stdout,
+#         error_fd=sys.stderr,
+#     )
+#     print("rc: {}".format(prestage_out))
+#     print("out: {}".format(prestage_err))
+#     print("err: {}".format(prestage_rc))
 
-if precheck_rc == 0 and prestage_rc == 0:
-    os_install_out, os_install_err, os_install_rc = ansible_runner.run_command(
-        executable_cmd='ansible-playbook',
-        cmdline_args=['/home/singhnavneet.su/os-upgrade/project/os_install.yaml',
-                      '-i', 'inventory', '-vvvv', '--tags', '9k'],
-        input_fd=sys.stdin,
-        output_fd=sys.stdout,
-        error_fd=sys.stderr,
-    )
-    print("rc: {}".format(os_install_out))
-    print("out: {}".format(os_install_err))
-    print("err: {}".format(os_install_rc))
+# if precheck_rc == 0 and prestage_rc == 0:
+#     os_install_out, os_install_err, os_install_rc = ansible_runner.run_command(
+#         executable_cmd='ansible-playbook',
+#         cmdline_args=['/home/singhnavneet.su/os-upgrade/project/os_install.yaml',
+#                       '-i', 'inventory', '-vvvv', '--tags', '9k'],
+#         input_fd=sys.stdin,
+#         output_fd=sys.stdout,
+#         error_fd=sys.stderr,
+#     )
+#     print("rc: {}".format(os_install_out))
+#     print("out: {}".format(os_install_err))
+#     print("err: {}".format(os_install_rc))
 
-time.sleep(600)
+# time.sleep(600)
 
-if precheck_rc == 0 and prestage_rc == 0 and os_install_rc == 0:
-    postcheck_out, postcheck_err, postcheck_rc = ansible_runner.run_command(
-        executable_cmd='ansible-playbook',
-        cmdline_args=['/home/singhnavneet.su/os-upgrade/project/postcheck.yaml',
-                      '-i', 'inventory', '-vvvv', '--tags', '9k'],
-        input_fd=sys.stdin,
-        output_fd=sys.stdout,
-        error_fd=sys.stderr,
-    )
-    print("rc: {}".format(postcheck_out))
-    print("out: {}".format(postcheck_err))
-    print("err: {}".format(postcheck_rc))
+# if precheck_rc == 0 and prestage_rc == 0 and os_install_rc == 0:
+#     postcheck_out, postcheck_err, postcheck_rc = ansible_runner.run_command(
+#         executable_cmd='ansible-playbook',
+#         cmdline_args=['/home/singhnavneet.su/os-upgrade/project/postcheck.yaml',
+#                       '-i', 'inventory', '-vvvv', '--tags', '9k'],
+#         input_fd=sys.stdin,
+#         output_fd=sys.stdout,
+#         error_fd=sys.stderr,
+#     )
+#     print("rc: {}".format(postcheck_out))
+#     print("out: {}".format(postcheck_err))
+#     print("err: {}".format(postcheck_rc))
 
 # if prestage_err == 0 and os_install_err == 0 and postcheck_err == 0:
 # if os_install_err == 0 and postcheck_err == 0:
